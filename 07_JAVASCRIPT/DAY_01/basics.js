ea804e37b2e59e4060cbf71975ceb229b0fd9c51
Brijosh <brijoshbaby@gmail.com>
2024-06-04 17:20:26 +0530

    js class update

---
+++ 07_JAVASCRIPT/DAY_01/basics.js
@@ -0,0 +1,32 @@
+                    // JavaScript Basics
+        // 1) Variables - named memory location;
+                    // variable declaration
+                    // a;
+                    var c;
+                    let e;
+
+                    // Variable initialization
+                    a=10;
+                    var hh=10;
+                    let b=10;
+                    const t=10;
+        // 2) Comment
+                    // 1) single line commenting
+                    // 2) Multi line comment  
+
+                    /*...........fgf
+                    dsfdsfdsfdsfsdfsd
+                    fgfdgdfvfssdfdsfs*/
+
+        // 3) Display statement usihng console.log
+                    //Print a message
+                    
+                    console.log("Hello World");
+
+                    console.log(123456);
+
+                    let message ="Hellow Woordld";
+                    console.log(message);
+
+                    let digits=213564;
+                    console.log(digits);
