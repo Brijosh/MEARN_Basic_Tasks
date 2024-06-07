e3db37ad3a8617e643fba4d2e7f943f24fab611b
Brijosh <brijoshbaby@gmail.com>
2024-06-07 17:12:54 +0530

    Conditional statements task

---
+++ 07_JAVASCRIPT/DAY_04/task.js
@@ -0,0 +1,17 @@
+var age=10;
+if(age>10 && age<20)
+    {
+        console.log("Age is between 10 and 20");
+    }
+else{
+    console.log("Age is not between 10 and 20");
+}
+
+var num=24;
+console.log(( num%2==0 && num%3==0)?'Number is divisible by 2 & 3':'Number is not divisible');
+
+var num=24;
+console.log(( num%2==0 || num%3==0)?'Number is divisible by 2 OR 3':'Number is not divisible by 2 OR');
+
+var age=24;
+console.log(( age>=18)?'You can Drive':'You Cannot Drive');
