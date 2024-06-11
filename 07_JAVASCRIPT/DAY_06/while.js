c20e4e4dd444d61712e42632c8a5a4b2b21c86a6
Brijosh <brijoshbaby@gmail.com>
2024-06-11 15:54:05 +0530

    while loops

---
+++ 07_JAVASCRIPT/DAY_06/while.js
@@ -0,0 +1,30 @@
+// print 1 to 5
+let limit=5;
+var i=1;
+while(i<=limit)
+    {
+        console.log(i);
+        i++;
+    }
+
+var num=1;
+while(num<=10)
+    {
+        if(num%2!=0)
+            {
+                console.log(num);
+            }
+        num++;
+    }
+
+
+var lmt=5;
+var fact=1;
+var i=1;
+while(i<=lmt)
+    {
+        fact=fact*i;
+        i++;
+    }
+
+console.log(fact);
